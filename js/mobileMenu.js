5af399186306607beeaf25f0caf0be7021fa8acd
JohnsonHuang1134 <danny8131@hotmail.com.tw>
2019-07-08 17:55:18 +0800

    <HotelBooking> Use simplify syntax

--- js/mobileMenu.js
+++ js/mobileMenu.js
@@ -1,9 +1,5 @@
 $(function(){
 	$("#menuBtn").on("click", function(){
-		if($("header > div").hasClass("open")){
-			$("header > div").removeClass("open");
-		}else{
-			$("header > div").addClass("open");
-		}
+		$("header > div").toggleClass("open");
 	})
 })
